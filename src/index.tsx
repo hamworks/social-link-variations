28a4d2a262f313e3d57e1522ccb732be925732da
Hiroshi Urabe <mail@torounit.com>
2025-02-06 03:21:38 +0900

    Add line css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { registerBlockVariation } from '@wordpress/blocks';
 import { LineIcon } from './icons/line';
 
+import './style.css';
+
 registerBlockVariation( 'core/social-link', {
 	name: 'line',
 	title: 'LINE',
